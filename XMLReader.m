b012b27642f561828c5793821ff901b2228f75a2
Evgeny Cherpak <evgeny@cloudengines.com>
2011-08-04 20:13:53 +0800

    Remove spaces and newline chars

--- XMLReader.m
+++ XMLReader.m
@@ -88,8 +88,18 @@ NSString *const kXMLReaderTextNodeKey = @"text";
 
 + (NSDictionary *)dictionaryForXMLString:(NSString *)string error:(NSError **)error
 {
-    NSData *data = [string dataUsingEncoding:NSUTF8StringEncoding];
+/*    NSData *data = [string dataUsingEncoding:NSUTF8StringEncoding];
     
+    return [XMLReader dictionaryForXMLData:data error:error];*/
+    NSArray* lines = [string componentsSeparatedByString:@"\n"];
+    NSMutableString* strData = [NSMutableString stringWithString:@""];
+    
+    for (int i = 0; i < [lines count]; i++)
+    {
+        [strData appendString:[[lines objectAtIndex:i] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]]];
+    }
+    
+    NSData *data = [strData dataUsingEncoding:NSUTF8StringEncoding];
     return [XMLReader dictionaryForXMLData:data error:error];
 }
 
